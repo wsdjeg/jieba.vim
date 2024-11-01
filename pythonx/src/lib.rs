04df2ab8f2a392b8a84aaec571d1f29cf5bb2667
kkew3 <kps6326@hotmail.com>
2024-11-01 11:14:01 +0800

    Rename main rust package

--- pythonx/src/lib.rs
+++ pythonx/src/lib.rs
@@ -4,7 +4,7 @@ use pyo3::prelude::*;
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn jieba_navi_rs(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn jieba_vim_rs(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<wrappers::WordMotionWrapper>()?;
 
     Ok(())
