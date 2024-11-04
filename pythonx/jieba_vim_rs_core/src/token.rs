73721d7b83ac06b439e7defd05652a283ca87eef
kkew3 <kps6326@hotmail.com>
2024-11-05 01:11:43 +0800

    Attach tsroten/zhon's license where it's quoted

--- pythonx/jieba_vim_rs_core/src/token.rs
+++ pythonx/jieba_vim_rs_core/src/token.rs
@@ -45,6 +45,30 @@ enum NonWordCharType {
 // The unicodes of CJK characters and punctuations are quoted from Github
 // repository: https://github.com/tsroten/zhon.
 // File: https://github.com/tsroten/zhon/blob/main/src/zhon/hanzi.py.
+// License: https://github.com/tsroten/zhon/blob/main/LICENSE.txt, attached
+// below:
+//
+// ---
+// Copyright (c) 2013-2014 Thomas Roten
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+// ---
 //
 // The partition of CJK punctuations into left/right/isolated types are decided
 // by myself, with help from https://www.compart.com/en/unicode. For CJK
