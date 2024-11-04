a9cfff51412b42d252d62341ea57c13a85727717
kkew3 <kps6326@hotmail.com>
2024-11-04 15:21:24 +0800

    Typo fix

--- pythonx/jieba_vim_rs_test_verifiable_case/src/lib.rs
+++ pythonx/jieba_vim_rs_test_verifiable_case/src/lib.rs
@@ -73,7 +73,7 @@ struct VerifiedCaseInput {
     before_cursor_position: CursorPosition,
     after_cursor_position: CursorPosition,
     buffers: Vec<String>,
-    stripped_beffers: Vec<String>,
+    stripped_buffers: Vec<String>,
     mode: Mode,
     operator: LitStr,
     motion: Motion,
@@ -175,7 +175,7 @@ impl Parse for VerifiedCaseInput {
             before_cursor_position: parsed_buffers.before_cursor_position,
             after_cursor_position: parsed_buffers.after_cursor_position,
             buffers,
-            stripped_beffers: parsed_buffers.striped_lines,
+            stripped_buffers: parsed_buffers.striped_lines,
             mode,
             operator,
             motion,
@@ -201,7 +201,7 @@ fn write_vader_given_block<W: Write>(
 
 impl VerifiedCaseInput {
     fn write_vader<W: Write>(&self, mut tofile: W) -> io::Result<()> {
-        let buffer_lines = &self.stripped_beffers;
+        let buffer_lines = &self.stripped_buffers;
         let lnum_before = self.before_cursor_position.lnum;
         let col_before = self.before_cursor_position.col + 1;
         let lnum_after = self.after_cursor_position.lnum;
